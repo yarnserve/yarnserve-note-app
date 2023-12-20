8e2d65349476ef4f3d90f281e605ab5e753aa3f0
yarnserve <yarnserve@gmail.com>
2023-12-20 10:38:22 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,110 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div className='App'>
+      <div className='header'>
+        <h1>📘 노트</h1>
+        <button>쓰기</button>
+      </div>
+      <ul className='list'>
+        <li>
+          <h2>노트 제목....</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos neque
+            officia optio? Unde quo consectetur omnis neque adipisci debitis
+            amet. Perspiciatis neque pariatur quidem? Voluptate voluptates iste
+            ipsa inventore atque?
+          </p>
+          <div className='note_footer'>
+            <div className='date'>2023-12-25 12:20:45</div>
+            <div className='btns'>
+              <button>수정</button>
+              <button>삭제</button>
+            </div>
+          </div>
+        </li>
+        <li>
+          <h2>노트 제목....</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos neque
+            officia optio? Unde quo consectetur omnis neque adipisci debitis
+            amet. Perspiciatis neque pariatur quidem? Voluptate voluptates iste
+            ipsa inventore atque?
+          </p>
+          <div className='note_footer'>
+            <div className='date'>2023-12-25 12:20:45</div>
+            <div className='btns'>
+              <button>수정</button>
+              <button>삭제</button>
+            </div>
+          </div>
+        </li>
+        <li>
+          <h2>노트 제목....</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos neque
+            officia optio? Unde quo consectetur omnis neque adipisci debitis
+            amet. Perspiciatis neque pariatur quidem? Voluptate voluptates iste
+            ipsa inventore atque?
+          </p>
+          <div className='note_footer'>
+            <div className='date'>2023-12-25 12:20:45</div>
+            <div className='btns'>
+              <button>수정</button>
+              <button>삭제</button>
+            </div>
+          </div>
+        </li>
+        <li>
+          <h2>노트 제목....</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos neque
+            officia optio? Unde quo consectetur omnis neque adipisci debitis
+            amet. Perspiciatis neque pariatur quidem? Voluptate voluptates iste
+            ipsa inventore atque?
+          </p>
+          <div className='note_footer'>
+            <div className='date'>2023-12-25 12:20:45</div>
+            <div className='btns'>
+              <button>수정</button>
+              <button>삭제</button>
+            </div>
+          </div>
+        </li>
+        <li>
+          <h2>노트 제목....</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos neque
+            officia optio? Unde quo consectetur omnis neque adipisci debitis
+            amet. Perspiciatis neque pariatur quidem? Voluptate voluptates iste
+            ipsa inventore atque?
+          </p>
+          <div className='note_footer'>
+            <div className='date'>2023-12-25 12:20:45</div>
+            <div className='btns'>
+              <button>수정</button>
+              <button>삭제</button>
+            </div>
+          </div>
+        </li>
+        <li>
+          <h2>노트 제목....</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Eos neque
+            officia optio? Unde quo consectetur omnis neque adipisci debitis
+            amet. Perspiciatis neque pariatur quidem? Voluptate voluptates iste
+            ipsa inventore atque?
+          </p>
+          <div className='note_footer'>
+            <div className='date'>2023-12-25 12:20:45</div>
+            <div className='btns'>
+              <button>수정</button>
+              <button>삭제</button>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+  )
+}
